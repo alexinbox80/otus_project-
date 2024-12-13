dc8f9d8a8d1b40f5824804c064fa074b86cd969d
AlekseySerov <alexinbox80@inbox.ru>
2024-12-13 15:31:33 +0300

    added examples to WorldController

--- src/Controller/WorldController.php
+++ src/Controller/WorldController.php
@@ -11,14 +11,26 @@ use Symfony\Component\HttpFoundation\Response;
 class WorldController extends AbstractController
 {
     public function __construct(
-        private readonly UserService $userService
+        private readonly UserService $userService,
+        private readonly UserBuilderService $userBuilderService
     )
     {
     }
 
     public function hello(): Response
     {
-        $users = $this->userService->findUsersByLoginWithCriteria('J.R.R. Tolkien');
+//        $user = $this->userBuilderService->createUserWithTweets(
+//            'J.R.R. Tolkien',
+//            ['The Hobbit', 'The Lord of the Rings']
+//        );
+
+        $users = $this->userBuilderService->createUserWithFollower(
+            'J.R.R. Tolkien',
+            'Ivan Ivanov'
+        );
+
+        //$users = $this->userService->findUsersByLogin('Ivan Ivanov');
+        //$users = $this->userService->findUsersByLoginWithCriteria('J.R.R. Tolkien');
 
         return $this->json(array_map(static fn(User $user) => $user->toArray(), $users));
     }
