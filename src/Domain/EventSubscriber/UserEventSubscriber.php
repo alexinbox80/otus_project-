220e77bdfc6a1e92570da6b2fb92847d9cbe9e80
AlekseySerov <alexinbox80@inbox.ru>
2025-01-30 00:44:49 +0300

    Grafana for metrics collection, integration with Graphite

--- src/Domain/EventSubscriber/UserEventSubscriber.php
+++ src/Domain/EventSubscriber/UserEventSubscriber.php
@@ -5,6 +5,7 @@ namespace App\Domain\EventSubscriber;
 use App\Domain\Event\CreateUserEvent;
 use App\Domain\Event\UserIsCreatedEvent;
 use App\Domain\Service\UserService;
+use App\Infrastructure\Storage\MetricsStorage;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -12,7 +13,8 @@ class UserEventSubscriber implements EventSubscriberInterface
 {
     public function __construct(
         private readonly UserService $userService,
-        private readonly LoggerInterface $logger,
+        private readonly LoggerInterface $elasticsearchLogger,
+        private readonly MetricsStorage $metricsStorage
     ) {
     }
 
@@ -39,6 +41,7 @@ class UserEventSubscriber implements EventSubscriberInterface
 
     public function onUserIsCreated(UserIsCreatedEvent $event): void
     {
-        $this->logger->info("User is created: id {$event->id}, login {$event->login}");
+        $this->elasticsearchLogger->info("User is created: id {$event->id}, login {$event->login}");
+        $this->metricsStorage->increment(MetricsStorage::USER_CREATED);
     }
 }
