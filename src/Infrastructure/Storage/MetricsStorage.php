341b28fb9a99299c62a79a298361dd27cd7e3a82
AlexeySerov <alexinbox80@inbox.ru>
2025-02-03 16:17:39 +0300

    Calculating cache hit/miss

--- src/Infrastructure/Storage/MetricsStorage.php
+++ src/Infrastructure/Storage/MetricsStorage.php
@@ -9,6 +9,10 @@ class MetricsStorage
 {
     public const USER_CREATED = 'user_created';
 
+    public const CACHE_HIT_PREFIX = 'cache.hit.';
+
+    public const CACHE_MISS_PREFIX = 'cache.miss.';
+
     private const DEFAULT_SAMPLE_RATE = 1.0;
 
     private Client $client;
